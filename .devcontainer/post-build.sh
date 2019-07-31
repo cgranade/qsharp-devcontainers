c3e7baad1f86fa820e32c6dbc0688ff9b731a634
Christopher Granade <cgranade@cgranade.com>
2019-07-31 01:02:45 +0000

    Fix some bugs in post-build.

--- .devcontainer/post-build.sh
+++ .devcontainer/post-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 2.2.401
 # Add the appropriate paths to $PATH through the bash profile.
-CAT << \EOF >> ~/.bash_profile
+cat << \EOF >> ~/.bash_profile
 export PATH="$HOME/.dotnet:$HOME/.dotnet/tools/:$PATH"
 EOF
 # Install required .NET Global Tools.
-dotnet install -g Microsoft.Quantum.IQSharp
-dotnet install -g PowerShell
+dotnet tool install -g Microsoft.Quantum.IQSharp
+dotnet tool install -g PowerShell
