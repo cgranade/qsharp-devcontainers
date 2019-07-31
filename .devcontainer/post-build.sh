f6363bb1386cb8c889cb1a5c5794048098f060a0
Christopher Granade <cgranade@cgranade.com>
2019-07-31 01:08:09 +0000

    Fixed paths a bit more.

--- .devcontainer/post-build.sh
+++ .devcontainer/post-build.sh
@@ -2,8 +2,13 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 2.2.401
 # Add the appropriate paths to $PATH through the bash profile.
 cat << \EOF >> ~/.bash_profile
-export PATH="$HOME/.dotnet:$HOME/.dotnet/tools/:$PATH"
+export DOTNET_ROOT="$HOME/.dotnet"
+export PATH="$DOTNET_ROOT:$DOTNET_ROOT/tools/:$PATH"
 EOF
+# Apply the bash profile now, to make sure we override any existing .NET Core installations.
+source ~/.bash_profile
 # Install required .NET Global Tools.
 dotnet tool install -g Microsoft.Quantum.IQSharp
 dotnet tool install -g PowerShell
+# Install required PowerShell modules for demos.
+pwsh -Command "Install-Module -Force posh-tex"
