3e54669328d1b4289675f8972fd1cc59768f47fb
Christopher Granade <cgranade@cgranade.com>
2019-07-31 00:59:53 +0000

    Register .NET Core SDK 2.2 on $PATH.

--- .devcontainer/post-build.sh
+++ .devcontainer/post-build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 2.2.401
+# Add the appropriate paths to $PATH through the bash profile.
+CAT << \EOF >> ~/.bash_profile
+export PATH="$HOME/.dotnet:$HOME/.dotnet/tools/:$PATH"
+EOF
